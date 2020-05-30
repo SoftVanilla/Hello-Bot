0f87d1b3187476282de98447c847e3526e7ace6a
SoftVanilla <52844822+SoftVanilla@users.noreply.github.com>
2020-05-30 15:24:43 +0900

    디스코드 봇 만들기 - 명렁어 만들기
    /주사위 명령어를 추가하는 과정

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
-import discord
+from discord.ext import commands
 
+import random
 
 # 개발자 페이지에서 봇에 대한 토큰 문자열을 가져온 뒤, TOKEN에 대입하자
 TOKEN = "봇 페이지에서 받은 토큰 문자열"
 
-client = discord.Client()
+client = commands.Bot(command_prefix='/')
 
 
-# 봇이 접속하면 아래의 함수를 실행하게 된다
 @client.event
 async def on_ready():
     print(f'{client.user} online!')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await message.channel.send("그렇군요!")
+@client.command(name='주사위')
+async def roll(ctx, number: int):
+    await ctx.send(f'주사위를 굴려 {random.randint(1,number)}이(가) 나왔습니다. (1-{number})')
 
 
-# 봇을 실행하자
+@roll.error
+async def roll_error(ctx, error):
+    await ctx.send(f"2 이상의 정수를 넣어주세요!\nex) /주사위 6")
+
+
 client.run(TOKEN)
